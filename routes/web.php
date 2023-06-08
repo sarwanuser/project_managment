0fad6482f43e659f2a34675311c28b5b8953d4a6
sarwanuser <sarwandeveloper@gmail.com>
2023-06-08 23:44:19 +0530

    update

--- routes/web.php
+++ routes/web.php
@@ -13,11 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
 
 
+
+Route::get('/', 'ProjectController@index');
 Route::get('/projects', 'ProjectController@index');
 Route::get('/projects/create', 'ProjectController@create');
 Route::get('/projects/{id}', 'ProjectController@show');
